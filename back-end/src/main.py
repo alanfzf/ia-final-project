fb7e9f20cbd7503987079149035146c04b045f57
alanfvn <alandavidg16@gmail.com>
2023-05-19 00:33:18 -0600

    Changed some code esp-rfid and python backend

--- back-end/src/main.py
+++ back-end/src/main.py
@@ -1,32 +1,41 @@
-import numpy as np
+# import numpy as np
+from dotenv import load_dotenv
 import cv2
+import os
 
-# cap = cv2.VideoCapture(0)
-cap = cv2.VideoCapture("http://192.168.0.118:81/stream")
+load_dotenv()
 
-face_cascade = cv2.CascadeClassifier(
-    f'{cv2.data.haarcascades}haarcascade_frontalface_default.xml')
+CAM_URL = os.getenv('ESPCAM_URL')
+
+print(CAM_URL)
+
+def recognize():
+    # cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(CAM_URL)
+
+    face_cascade = cv2.CascadeClassifier(
+        f'{cv2.data.haarcascades}haarcascade_frontalface_default.xml')
 
 
-while(True):
-    ret, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray,
-                                          scaleFactor=1.5, minNeighbors=5)
+    while(True):
+        ret, frame = cap.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray,
+                                              scaleFactor=1.5, minNeighbors=5)
 
-    for (x, y, w, h) in faces:
-        print(x, y, h, w)
+        for (x, y, w, h) in faces:
+            print(x, y, h, w)
 
-        color = (255, 0, 0)
-        stroke = 2
-        end_coord_x = x+w
-        end_coord_y = y+h
+            color = (255, 0, 0)
+            stroke = 2
+            end_coord_x = x+w
+            end_coord_y = y+h
 
-        cv2.rectangle(frame, (x, y), (end_coord_x, end_coord_y), color, stroke)
-    cv2.imshow('frame', frame)
+            cv2.rectangle(frame, (x, y), (end_coord_x, end_coord_y), color, stroke)
+        cv2.imshow('frame', frame)
 
-    if cv2.waitKey(20) & 0xFF == ord('q'):
-        break
+        if cv2.waitKey(20) & 0xFF == ord('q'):
+            break
 
-cap.release()
-cv2.destroyAllWindows()
+    cap.release()
+    cv2.destroyAllWindows()
